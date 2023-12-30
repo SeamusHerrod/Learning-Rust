93addd9f5088cc17f2fe309c5b517700fcdea370
SeamusHerrod <85411251+SeamusHerrod@users.noreply.github.com>
2023-12-30 10:41:02 -0600

    rust Hello world and intro to cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-9cb0aac463825894.d
@@ -0,0 +1,5 @@
+/Users/sharkbait/Desktop/firstRust/hello_cargo/target/debug/deps/libhello_cargo-9cb0aac463825894.rmeta: src/main.rs
+
+/Users/sharkbait/Desktop/firstRust/hello_cargo/target/debug/deps/hello_cargo-9cb0aac463825894.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/sharkbait/Desktop/firstRust/hello_cargo/target/debug/hello_cargo: /Users/sharkbait/Desktop/firstRust/hello_cargo/src/main.rs
